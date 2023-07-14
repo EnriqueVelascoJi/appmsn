17fabc46733f82bb51decf052d4be7966b3dc023
EnriqueVelascoJi <70172210+EnriqueVelascoJi@users.noreply.github.com>
2023-07-14 13:37:57 -0600

    Update postgres.js

--- back/src/DB/postgres.js
+++ back/src/DB/postgres.js
@@ -3,8 +3,8 @@ const sessionPool = require('pg').Pool
 
 const pool = new sessionPool({
     user: 'postgres',
-    password: 'yE3$m%aj5UCeT5ca',
-    host: 'msn.cvofrq8gjce2.us-east-2.rds.amazonaws.com',
+    password: 't8kE36ZR^mPKLCaC',
+    host: 'appmsn.cvofrq8gjce2.us-east-2.rds.amazonaws.com',
     port: '5434',
     database: 'postgres'
 })
