e3c91d60dc894af2b5e2feeda7d2b43c103b4fb9
EnriqueVelascoJi <70172210+EnriqueVelascoJi@users.noreply.github.com>
2023-07-17 10:20:20 -0600

    Update Mecanico.Route.js

--- back/src/routes/Mecanico.Route.js
+++ back/src/routes/Mecanico.Route.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 
-const { get_all_mecanicos, create_mecanico, update_mecanico, delete_mecanico} = require('../controllers/Mecanico.Controller');
+const { get_all_mecanicos, create_mecanico, delete_mecanico, get_mecanico, update_mecanico} = require('../controllers/Mecanico.Controller');
 const router = Router();
 
 
@@ -8,5 +8,8 @@ router.route('/').get(get_all_mecanicos);
 router.route('/').post(create_mecanico);
 router.route('/:id').put(update_mecanico);
 router.route('/:id').patch(delete_mecanico);
+router.route('/:id').get(get_mecanico) ;
+router.route('/:id').patch(update_mecanico);
+
 
 module.exports = router;
