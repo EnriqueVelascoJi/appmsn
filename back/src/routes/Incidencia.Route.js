f80323826699b1423ed2cdef672329bdfcabc384
EnriqueVelascoJi <70172210+EnriqueVelascoJi@users.noreply.github.com>
2023-07-25 05:31:07 -0600

    Update Incidencia.Route.js

--- back/src/routes/Incidencia.Route.js
+++ back/src/routes/Incidencia.Route.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 
-const { get_all_incidencias, get_incidencia,create_incidencia, update_incidencia, delete_incidencia, get_resumen1, get_resumen2, get_resumen3, ver_mas, get_by_equipo} = require('../controllers/Incidencia.Controller');
+const { get_all_incidencias, get_incidencia,create_incidencia, update_incidencia, delete_incidencia, get_resumen1, get_resumen2, get_resumen3, ver_mas, get_by_equipo, get_by_equipos, get_by_aeropuertos} = require('../controllers/Incidencia.Controller');
 const router = Router();
 
 
@@ -14,5 +14,7 @@ router.route('/:id').patch(delete_incidencia);
 router.route('/costos1').post(get_resumen1);
 router.route('/costos2').post(get_resumen2);
 router.route('/costos3').post(get_resumen3);
+router.route('/equipos').post(get_by_equipos);
+router.route('/aeropuertos').post(get_by_aeropuertos);
 
 module.exports = router;
