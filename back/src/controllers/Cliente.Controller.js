d84459280254eab54549a8e7debcc9596255d745
EnriqueVelascoJi <70172210+EnriqueVelascoJi@users.noreply.github.com>
2023-07-31 15:54:05 -0600

    Update Cliente.Controller.js

--- back/src/controllers/Cliente.Controller.js
+++ back/src/controllers/Cliente.Controller.js
@@ -5,7 +5,7 @@ const pool = require('../DB/postgres');
 //Get all users
 exports.get_all_clientes = async (req, res) => {
 
-    const query = 'SELECT * FROM cliente';
+    const query = 'SELECT * FROM cliente where isdeleted=FALSE order by idcliente';
     
     // Get all
     const response = await pool.query(query);
@@ -25,7 +25,8 @@ exports.create_cliente= async (req, res) => {
 
     let {
         nombre,
-        descripcion
+        descripcion,
+        aeropuertos
     } = req.body 
 
     try{
@@ -36,6 +37,15 @@ exports.create_cliente= async (req, res) => {
             nombre,
             descripcion
         ]);
+    const idCliente = response.rows[0].idcliente;
+
+    let data = ''
+    for(let i = 0; i < aeropuertos.length; i++) {
+        data += `(false,${idCliente},${aeropuertos[i].idaeropuerto}),`
+    }
+    const queryCA = `INSERT INTO cliente_aeropuerto(isdeleted,idcliente,idaeropuerto) values${data}`;
+    const parseQueryCA = queryCA.substring(0, queryCA.length - 1);
+    var response2 = await pool.query(parseQueryCA);
             
         res
         .status(201)
@@ -71,21 +81,21 @@ exports.update_cliente = async(req, res) => {
 }
 exports.delete_cliente = async(req, res) => {
 
-    try{
-        const updatedCliente = await Cliente.findByIdAndUpdate(req.params.id,req.body,{
-            new : true,
-            runValidators : true
-        })
+    const id = req.params.id;
+    const query = 'UPDATE cliente SET isdeleted=TRUE WHERE idcliente=$1;';
+
+    // Create
+    const response = await pool.query(query, [
+        id
+    ]);
         
-        res.status(200).json({
-            status : 'Success',
-            data : {
-              updatedCliente
-            }
-        })
-    }catch(err){
-        console.log(err)
-    }
+    res
+    .status(201)
+    .json({
+      status: "success",
+      msg: "Recording sucessfully",
+      data: req.body
+    })
     
     
 }
