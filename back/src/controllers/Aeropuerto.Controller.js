c97f42f8d74fad4375c97e7c2b0a0b826aa87a14
EnriqueVelascoJi <70172210+EnriqueVelascoJi@users.noreply.github.com>
2023-08-08 08:56:45 -0600

    Update Aeropuerto.Controller.js

--- back/src/controllers/Aeropuerto.Controller.js
+++ back/src/controllers/Aeropuerto.Controller.js
@@ -43,6 +43,30 @@ exports.get_aeropuerto = async (req, res) => {
     .end()
 
 }
+
+exports.get_aeropuertos_by_cliente = async (req, res) => {
+
+    const cliente = req.params.cliente;
+    const query = `select a.idaeropuerto, a.nombre, a.siglas, a.isdeleted, ce.idclienteaeropuerto from cliente_aeropuerto ce
+    inner join aeropuerto a
+    on a.idaeropuerto = ce.idaeropuerto
+    where ce.idcliente=$1`;
+    
+    // Get all aeropuertos
+    const response = await pool.query(query, [cliente]);
+
+    console.log(response);
+    
+    res
+    .status(201)
+    .json({ 
+      status: "success",
+      msg: "Recording sucessfully",
+      data: response.rows
+    })
+    .end()
+
+}
 exports.create_aeropuerto= async (req, res) => {
 
     const data = req.body;
