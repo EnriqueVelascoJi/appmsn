8fb467ffa18be029b21fcadbbc5d8d2679b73141
EnriqueVelascoJi <70172210+EnriqueVelascoJi@users.noreply.github.com>
2023-08-10 15:28:32 -0600

    Update Equipo.Route.js

--- back/src/routes/Equipo.Route.js
+++ back/src/routes/Equipo.Route.js
@@ -5,6 +5,7 @@ const router = Router();
 
 
 router.route('/').get(get_all_equipos);
+router.route('/tipoequipos').get(get_all_equipos);
 router.route('/:id').get(get_equipo);
 router.route('/').post(create_equipo);
 router.route('/:id').patch(update_equipo);
