5637572b491122ceca7ad1e070da1546a66f167e
EnriqueVelascoJi <70172210+EnriqueVelascoJi@users.noreply.github.com>
2023-08-15 12:53:07 -0600

    Update Equipo.Route.js

--- back/src/routes/Equipo.Route.js
+++ back/src/routes/Equipo.Route.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 
-const { get_all_equipos,create_equipo, update_equipo, delete_equipo, get_equipo, get_all_tipos_equipos} = require('../controllers/Equipo.Controller');
+const { get_all_equipos,create_equipo, update_equipo, delete_equipo, get_equipo, get_all_tipos_equipos, get_equipo_by_clienteaeropuerto} = require('../controllers/Equipo.Controller');
 const router = Router();
 
 
 router.route('/').get(get_all_equipos);
 router.route('/tipoequipos').get(get_all_tipos_equipos);
+router.route('/equipos/:id').get(get_equipo_by_clienteaeropuerto);
 router.route('/:id').get(get_equipo);
 router.route('/').post(create_equipo);
 router.route('/:id').patch(update_equipo);
