bb5636cb440fc82041bb634aa310dbbe93263e2a
EnriqueVelascoJi <70172210+EnriqueVelascoJi@users.noreply.github.com>
2023-08-25 15:35:15 -0600

    Update Incidencia.Route.js

--- back/src/routes/Incidencia.Route.js
+++ back/src/routes/Incidencia.Route.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 
-const { get_all_incidencias, get_incidencia,create_incidencia, update_incidencia, delete_incidencia, get_resumen1, get_resumen2, get_resumen3, ver_mas, get_by_equipo, get_by_equipos, get_by_aeropuertos, get_by_date} = require('../controllers/Incidencia.Controller');
+const { get_all_incidencias, get_incidencia,create_incidencia, update_incidencia, delete_incidencia, get_resumen1, get_resumen2, get_resumen3, ver_mas, get_by_equipo, get_by_equipos, get_by_aeropuertos, get_by_date, get_by_clientes, get_resumen4} = require('../controllers/Incidencia.Controller');
 const router = Router();
 
 
@@ -15,7 +15,10 @@ router.route('/:id').delete(delete_incidencia);
 router.route('/costos1').post(get_resumen1);
 router.route('/costos2').post(get_resumen2);
 router.route('/costos3').post(get_resumen3);
-router.route('/equipos').post(get_by_equipos);
+router.route('/costos4').post(get_resumen4);
+router.route('/equipos/').get(get_by_equipos);
 router.route('/aeropuertos').post(get_by_aeropuertos);
+router.route('/clientes').post(get_by_clientes);
+
 
 module.exports = router;
