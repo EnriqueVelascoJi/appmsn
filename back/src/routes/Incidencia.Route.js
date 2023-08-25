e31e0891954cdd6cc2f39eac691faf9cdbd568b9
EnriqueVelascoJi <70172210+EnriqueVelascoJi@users.noreply.github.com>
2023-08-25 08:17:26 -0600

    Update Incidencia.Route.js

--- back/src/routes/Incidencia.Route.js
+++ back/src/routes/Incidencia.Route.js
@@ -9,7 +9,7 @@ router.route('/:id').get(get_incidencia);
 router.route('/getbyequipo/:id').get(get_by_equipo);
 router.route('/vermas/:id').get(ver_mas);
 router.route('/').post(create_incidencia);
-router.route('/:id').put(update_incidencia);
+router.route('/:id').patch(update_incidencia);
 router.route('/:id').delete(delete_incidencia);
 router.route('/costos1').post(get_resumen1);
 router.route('/costos2').post(get_resumen2);
