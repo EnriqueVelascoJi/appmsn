8db01ded4f5df611d7368bedfe55e0211e53edcc
EnriqueVelascoJi <70172210+EnriqueVelascoJi@users.noreply.github.com>
2023-08-25 11:00:50 -0600

    Update Incidencia.Controller.js

--- back/src/controllers/Incidencia.Controller.js
+++ back/src/controllers/Incidencia.Controller.js
@@ -110,9 +110,7 @@ exports.get_by_date = async (req, res) => {
 
     const date1 = new Date(fechaInicio).toISOString().slice(0, 10)
     const date2 = new Date(fechaFin).toISOString().slice(0, 10)
-    const query = `select *,i.nombre nombreincidencia, r.nombre nombrerefaccion from incidencia i 
-    inner join refacciones_incidencia ri on i.idincidencia = ri.idincidencia
-    inner join refaccion r on ri.idrefaccion = r.idrefaccion where i.fecha >= $1 AND i.fecha <= $2`
+    const query = `select * from incidencia where fecha >= $1 AND fecha <= $2`
         var response = await pool.query(query, [ date1, date2]);
       
         if(response.rows.length == 0){
@@ -304,7 +302,8 @@ exports.get_resumen1 = async (req, res) => {
 
     const date1 = new Date(fechaInicio).toISOString().slice(0, 10)
     const date2 = new Date(fechaFin).toISOString().slice(0, 10)
-    const query = `select ri.idrefaccionesincidencia, ri.nopiezas, ri.costo, ri.precioventa from incidencia i inner join refacciones_incidencia ri on i.idincidencia = ri.idincidencia where i.fecha >= $1 AND i.fecha <= $2`
+    const query = `select ri.idrefaccionesincidencia, ri.nopiezas, ri.costo, ri.precioventa from incidencia i 
+    inner join refacciones_incidencia ri on i.idincidencia = ri.idincidencia where i.fecha >= $1 AND i.fecha <= $2`
     console.log(date1)
    
 
