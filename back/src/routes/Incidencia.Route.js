c23dfc25fcbf0060098647ced9349d72c3c84985
EnriqueVelascoJi <70172210+EnriqueVelascoJi@users.noreply.github.com>
2023-09-08 08:12:33 -0600

    Update Incidencia.Route.js

--- back/src/routes/Incidencia.Route.js
+++ back/src/routes/Incidencia.Route.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 
-const { get_all_incidencias, get_incidencia,create_incidencia, update_incidencia, delete_incidencia, get_resumen1, get_resumen2, get_resumen3, ver_mas, get_by_equipo, get_by_equipos, get_by_aeropuertos, get_by_date, get_by_clientes, get_resumen4, uploadImages, uploadFiles, ver_files, ver_imagenes} = require('../controllers/Incidencia.Controller');
+const { get_all_incidencias, get_incidencia,create_incidencia, update_incidencia, delete_incidencia, get_resumen1, get_resumen2, get_resumen3, ver_mas, get_by_equipo, get_by_equipos, get_by_aeropuertos, get_by_date, get_by_clientes, get_resumen4, uploadImages, uploadFiles, ver_files, ver_imagenes, borrarImages, borrarFiles} = require('../controllers/Incidencia.Controller');
 const router = Router();
 
 
@@ -23,7 +23,8 @@ router.route('/aeropuertos').post(get_by_aeropuertos);
 router.route('/clientes').post(get_by_clientes);
 router.route('/uploadimages').post(uploadImages);
 router.route('/uploadfiles').post(uploadFiles);
-
+router.route('/borrarimages').post(borrarImages);
+router.route('/borrarfiles').post(borrarFiles);
 
 
 module.exports = router;
