468da437c674cfda69d68bc16e8840aff36a0ae1
EnriqueVelascoJi <70172210+EnriqueVelascoJi@users.noreply.github.com>
2024-11-07 10:32:57 -0600

    Update Usuario.Route.js

--- back/src/routes/Usuario.Route.js
+++ back/src/routes/Usuario.Route.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 
-const { get_all_usuarios, create_usuario, update_usuario, delete_usuario, login, get_usuario} = require('../controllers/Usuario.Controller');
+const { get_all_usuarios, create_usuario, update_usuario, delete_usuario, login, get_usuario, get_all_usuarios_gd, create_usuario_gd, update_usuario_gd, login_gd, get_usuario_gd} = require('../controllers/Usuario.Controller');
 const router = Router();
 
 
@@ -9,6 +9,13 @@ router.route('/:id').get(get_usuario) ;
 router.route('/').post(create_usuario);
 router.route('/login').post(login);
 router.route('/:id').patch(update_usuario);
-router.route('/:id').delete(delete_usuario);  
+router.route('/:id').delete(delete_usuario);
+
+router.route('/gd/').get(get_all_usuarios_gd);
+router.route('/gd/:id').get(get_usuario_gd);
+router.route('/gd/').post(create_usuario_gd);
+router.route('/gd/login').post(login_gd);
+router.route('/gd/:id').patch(update_usuario_gd);
+
 
 module.exports = router;
