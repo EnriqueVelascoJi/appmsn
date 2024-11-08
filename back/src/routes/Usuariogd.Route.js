e85657a494a03ef65c2a6b7909e323a5e08e9b02
EnriqueVelascoJi <70172210+EnriqueVelascoJi@users.noreply.github.com>
2024-11-07 18:43:25 -0600

    Update Usuariogd.Route.js

--- back/src/routes/Usuariogd.Route.js
+++ back/src/routes/Usuariogd.Route.js
@@ -1,16 +1,17 @@
 
 const { Router } = require('express');
 
-const { get_all_usuarios_gd, create_usuario_gd, update_usuario_gd, login_gd, get_usuario_gd, create_project_gd } = require('../controllers/Usuario.Controller');
+const { get_all_usuarios_gd, create_usuario_gd, update_usuario_gd, login_gd, get_usuario_gd, create_project_gd, get_projects_gd } = require('../controllers/Usuario.Controller');
 const router = Router();
 
 
 router.route('/').get(get_all_usuarios_gd);
-router.route('/:id').get(get_usuario_gd);
+//router.route('/:id').get(get_usuario_gd);
 router.route('/').post(create_usuario_gd);
 router.route('/login').post(login_gd);
 router.route('/:id').patch(update_usuario_gd);
-router.route('/project').post(create_project_gd)
+router.route('/project').post(create_project_gd);
+router.route('/projects').get(get_projects_gd);
 
 
 module.exports = router;
