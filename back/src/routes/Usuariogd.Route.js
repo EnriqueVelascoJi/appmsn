a5b26cf13431781251d89c3684dd46c111265932
EnriqueVelascoJi <70172210+EnriqueVelascoJi@users.noreply.github.com>
2024-11-11 10:34:13 -0600

    Update Usuariogd.Route.js

--- back/src/routes/Usuariogd.Route.js
+++ back/src/routes/Usuariogd.Route.js
@@ -6,7 +6,7 @@ const router = Router();
 
 
 router.route('/').get(get_all_usuarios_gd);
-//router.route('/:id').get(get_usuario_gd);
+router.route('/usuariogd/:id').get(get_usuario_gd);
 router.route('/').post(create_usuario_gd);
 router.route('/login').post(login_gd);
 router.route('/:id').patch(update_usuario_gd);
