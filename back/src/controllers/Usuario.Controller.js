e6a4e864ae9e7145cf2ca0bf2f38aaf717842788
EnriqueVelascoJi <70172210+EnriqueVelascoJi@users.noreply.github.com>
2024-11-11 13:04:52 -0600

    Update Usuario.Controller.js

--- back/src/controllers/Usuario.Controller.js
+++ back/src/controllers/Usuario.Controller.js
@@ -399,10 +399,10 @@ exports.get_process_gd = async(req, res) => {
     let queryProject = ''
 
   if(idRequirement) {
-        queryProject = 'SELECT * FROM processgd pgd inner join projectgd prgd on pgd.idproject = prgd.id inner join requirementgd rgd on pgd.idrequirement = rgd.id inner join usuariogd ugd  on pgd.idusuario = ugd.id inner join statusgd sgd on pgd.idstatus = sgd.idstatus WHERE idusuario=$1 order by pgd.id';
+        queryProject = 'SELECT * FROM processgd pgd inner join projectgd prgd on pgd.idproject = prgd.id inner join requirementgd rgd on pgd.idrequirement = rgd.id inner join usuariogd ugd  on pgd.idusuario = ugd.id inner join statusgd sgd on pgd.idstatus = sgd.id WHERE idusuario=$1 order by pgd.id';
 
     } else  {
-        queryProject = 'select * from processgd pgd inner join projectgd prgd on pgd.idproject = prgd.id inner join usuariogd ugd  on pgd.idusuario = ugd.id inner join statusgd sgd on pgd.idstatus = sgd.idstatus WHERE idusuario=$1 order by pgd.id'
+        queryProject = 'select * from processgd pgd inner join projectgd prgd on pgd.idproject = prgd.id inner join usuariogd ugd  on pgd.idusuario = ugd.id inner join statusgd sgd on pgd.idstatus = sgd.id WHERE idusuario=$1 order by pgd.id'
     }
     
 
