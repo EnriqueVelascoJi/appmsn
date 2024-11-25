53a22cf74c99437db54b215b28f1b93fafde7a56
EnriqueVelascoJi <70172210+EnriqueVelascoJi@users.noreply.github.com>
2024-11-25 13:57:07 -0600

    Update Usuariogd.Route.js

--- back/src/routes/Usuariogd.Route.js
+++ back/src/routes/Usuariogd.Route.js
@@ -1,7 +1,7 @@
 
 const { Router } = require('express');
 
-const { get_all_usuarios_gd, create_usuario_gd, update_usuario_gd, login_gd, get_usuario_gd, create_project_gd, get_projects_gd, get_partial_projects_gd, get_all_notifications_gd, get_notifications_by_user_gd, get_process_gd, create_requirement_gd, update_project_gd, create_dict_procedure, update_dict_procedure, create_project,get_projects, get_project, update_status_project } = require('../controllers/Usuario.Controller');
+const { get_all_usuarios_gd, create_usuario_gd, update_usuario_gd, login_gd, get_usuario_gd, create_project_gd, get_projects_gd, get_partial_projects_gd, get_all_notifications_gd, get_notifications_by_user_gd, get_process_gd, create_requirement_gd, update_project_gd, create_dict_procedure, update_dict_procedure, create_project,get_projects, get_project, update_status_project,update_project } = require('../controllers/Usuario.Controller');
 const router = Router();
 
 
@@ -12,6 +12,7 @@ router.route('/login').post(login_gd);
 //router.route('/:id').patch(update_usuario_gd);
 router.route('/project').post(create_project);
 router.route('/project/:id').get(get_project);
+router.route('/project/:id').patch(update_project);
 router.route('/requirement').post(create_requirement_gd);
 router.route('/projects').get(get_projects_gd);
 router.route('/projects').patch(update_project_gd);
