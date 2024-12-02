aaae7a0f42ad7fe6cc35f1820304e0c60898de77
EnriqueVelascoJi <70172210+EnriqueVelascoJi@users.noreply.github.com>
2024-12-02 10:18:45 -0600

    Update Usuariogd.Route.js

--- back/src/routes/Usuariogd.Route.js
+++ back/src/routes/Usuariogd.Route.js
@@ -1,7 +1,7 @@
 
 const { Router } = require('express');
 
-const { get_all_usuarios_gd, create_usuario_gd, update_usuario_gd, login_gd, get_usuario_gd, create_project_gd, get_projects_gd, get_partial_projects_gd, get_all_notifications_gd, get_notifications_by_user_gd, get_process_gd, create_requirement_gd, update_project_gd, create_dict_procedure, update_dict_procedure, create_project,get_projects, get_project, update_status_project,update_project, create_glosary, get_glosary_terms, get_complete, get_glosary,update_glosary } = require('../controllers/Usuario.Controller');
+const { get_all_usuarios_gd, create_usuario_gd, update_usuario_gd, login_gd, get_usuario_gd, create_project_gd, get_projects_gd, get_partial_projects_gd, get_all_notifications_gd, get_notifications_by_user_gd, get_process_gd, create_requirement_gd, update_project_gd, create_dict_procedure, update_dict_procedure, create_project,get_projects, get_project, update_status_project,update_project, create_glosary, get_glosary_terms, get_complete, get_glosary,update_glosary, get_status } = require('../controllers/Usuario.Controller');
 const router = Router();
 
 
@@ -27,6 +27,7 @@ router.route('/glosary').post(create_glosary);
 router.route('/glosary').get(get_glosary);
 router.route('/glosary/:id').get(get_glosary_terms);
 router.route('/complete/:id').get(get_complete);
+router.route('/status/:id').get(get_status);
 router.route('/glosary/:id').patch(update_glosary);
 
 
